8d48514c956a5dc31752114c4b3b23ed4b6603b1
hyoinkang <hyoin1704@gmail.com>
2024-07-15 05:20:30 +0900

    initial commit

---
+++ src/main/java/umc/ShowHoo/ShowHooApplication.java
@@ -0,0 +1,13 @@
+package umc.ShowHoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowHooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowHooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/ShowHoo/ShowHooApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.ShowHoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
