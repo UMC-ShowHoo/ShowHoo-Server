bb0ecb36507c132ce0e8b4ea63c981f53fb38889
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-07-27 15:24:29 +0900

    chore:s3 업로드 테스트 예시 api #20

--- src/main/java/umc/ShowHoo/web/performerProfile/controller/PerformerProfileController.java
+++ src/main/java/umc/ShowHoo/web/performerProfile/controller/PerformerProfileController.java
@@ -1,16 +1,21 @@
 package umc.ShowHoo.web.performerProfile.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import umc.ShowHoo.aws.s3.AmazonS3Manager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import umc.ShowHoo.apiPayload.ApiResponse;
+import umc.ShowHoo.aws.s3.Uuid;
+import umc.ShowHoo.aws.s3.UuidRepository;
 import umc.ShowHoo.web.performerProfile.dto.PerformerProfileRequestDTO;
-import umc.ShowHoo.web.performerProfile.entity.PerformerProfile;
 import umc.ShowHoo.web.performerProfile.service.PerformerProfileService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +35,19 @@ public class PerformerProfileController {
 
         performerProfileService.createProfile(performerUserId, profileDTO, profileImages);
         return ApiResponse.onSuccess(null);
+    }
+
+    //예시
+    private final AmazonS3Manager s3Manager;
+    private final UuidRepository uuidRepository;
+
+    @PostMapping(value = "/example", consumes = "multipart/form-data")
+    public String example(@RequestParam("profileImage") MultipartFile profileImage){
+        String uuid = UUID.randomUUID().toString();
+        Uuid savedUuid = uuidRepository.save(Uuid.builder()
+                .uuid(uuid).build());
+        String pictureUrl = s3Manager.uploadFile(s3Manager.generatePerformerProfileImageKeyName(savedUuid), profileImage);
+        return pictureUrl;
     }
 
 }
