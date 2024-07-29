4c46e9eda65b72c3ed209af95ed6243df8331803
dldusgh318 <pololydotoly@gmail.com>
2024-07-29 13:41:33 +0900

    feat: 공연자/공연장 공연준비 - 요청 사항, 공연 포스터/티켓 발행 API 구현 (#19)

--- src/main/java/umc/ShowHoo/web/Shows/repository/ShowsRepository.java
+++ src/main/java/umc/ShowHoo/web/Shows/repository/ShowsRepository.java
@@ -2,7 +2,9 @@ package umc.ShowHoo.web.Shows.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import umc.ShowHoo.web.Shows.entity.Shows;
 
+@Repository
 public interface ShowsRepository extends JpaRepository<Shows, Long> {
 }
--- src/main/java/umc/ShowHoo/web/performer/controller/PerformerController.java
+++ src/main/java/umc/ShowHoo/web/performer/controller/PerformerController.java
@@ -1,11 +1,8 @@
 package umc.ShowHoo.web.performer.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import umc.ShowHoo.apiPayload.ApiResponse;
 import umc.ShowHoo.web.performer.repository.PerformerRepository;
 import umc.ShowHoo.web.performer.service.PerformerService;
 
@@ -17,10 +14,5 @@ public class PerformerController {
     private final PerformerService performerService;
     private final PerformerRepository performerRepository;
 
-//    @PostMapping("/performer/")
-//    @Operation(summary = "공연자 공연 준비- 큐시트 확인 api",description = "공연 준비중 큐시트 확인 시에 필요한 API")
-//    @ApiResponses({
-//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK, 성공")
-//    })
-//    public ApiResponse<>
+
 }
