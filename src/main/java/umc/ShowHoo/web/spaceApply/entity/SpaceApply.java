8cd61b0b854acc3a68d70bf5bd19016c081a273a
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-07-31 13:02:27 +0900

    feat: 선택된 추가서비스 엔티티 생성 #27

--- src/main/java/umc/ShowHoo/web/spaceApply/entity/SpaceApply.java
+++ src/main/java/umc/ShowHoo/web/spaceApply/entity/SpaceApply.java
@@ -3,9 +3,12 @@ package umc.ShowHoo.web.spaceApply.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import umc.ShowHoo.web.performer.entity.Performer;
+import umc.ShowHoo.web.selectedAdditionalService.entity.SelectedAdditionalService;
 import umc.ShowHoo.web.space.entity.Space;
+import umc.ShowHoo.web.spacePrefer.entity.SpacePrefer;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,7 +24,7 @@ public class SpaceApply {
     private int audienceMin; //관람객 최소인원
     private int audienceMax; //관람객 최대인원
     private Long performerProfileId; //공연자 프로필 id
-    private boolean status; //대관승인상태
+    private int status; //대관승인상태 - 대관승인상태가 대관신청, 대관완료, 공연완료, 대관신청거절 4가지
 
     @ManyToOne
     @JoinColumn(name = "performer_id")
@@ -30,4 +33,7 @@ public class SpaceApply {
     @ManyToOne
     @JoinColumn(name = "space_id")
     private Space space;
+
+    @OneToMany(mappedBy = "spaceApply", cascade = CascadeType.ALL)
+    private List<SelectedAdditionalService> selectedAdditionalServices;
 }
