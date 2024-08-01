1fa6a8b50632266b05d59522db754545e839c150
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-08-01 23:18:44 +0900

    chore: 리뷰 작성 api 설명 추가 #41

--- src/main/java/umc/ShowHoo/web/spaceReview/controller/SpaceReviewController.java
+++ src/main/java/umc/ShowHoo/web/spaceReview/controller/SpaceReviewController.java
@@ -1,5 +1,10 @@
 package umc.ShowHoo.web.spaceReview.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +23,16 @@ public class SpaceReviewController {
     private final SpaceReviewService spaceReviewService;
 
 
+    @Operation(summary = "공연자 리뷰 작성 API", description = "공연자가 공연장에 대한 리뷰를 작성할 때 필요한 API입니다.")
+    @Parameter(
+            in = ParameterIn.HEADER,
+            name = "Authorization", required = true,
+            schema = @Schema(type = "string"),
+            description = "Bearer [Access 토큰]"
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK, 성공"),
+    })
     @PostMapping(value = "/spaces/{spaceId}/review/{performerId}", consumes = "multipart/form-data")
     public ApiResponse<Void> createSpaceReview(
             @PathVariable Long spaceId,
