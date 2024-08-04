d2f68ee2b96a136e742df5317f6cd96a5ac25011
hyoinkang <hyoin1704@gmail.com>
2024-08-04 17:06:26 +0900

    feature : 카카오 연결 끊기 기능 구현

--- src/main/java/umc/ShowHoo/web/login/controller/LoginController.java
+++ src/main/java/umc/ShowHoo/web/login/controller/LoginController.java
@@ -72,4 +72,25 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+    //카카오 연결 끊기(by accessToken)
+    @GetMapping("/kakao/delete")
+    public String kakaoUnlink(HttpSession session) {
+        String accessToken = (String) session.getAttribute("kakaoToken");
+        System.out.println(accessToken);
+
+        if(accessToken != null && !"".equals(accessToken)){
+            try {
+                kakaoService.KaKaoUnlink(accessToken);
+            } catch (JsonProcessingException e){
+                throw new RuntimeException(e);
+            }
+            session.removeAttribute("kakaoToken");
+            session.removeAttribute("loginMember");
+        }else {
+            System.out.println("accessToken is null");
+        }
+
+        return "redirect:/";
+    }
 }
