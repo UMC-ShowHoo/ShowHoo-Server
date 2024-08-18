5b5f0f2de3de4cc02f391a1b808690e0b0b55b51
hyesuhan <121243582+hyesuhan@users.noreply.github.com>
2024-08-19 08:26:09 +0900

    Revert "fix : 대관 가능 날싸 확인 API를 GETMAPPING (#121)"
    This reverts commit 1ea22af228929347becf0e8b0aa968ed7907fcce.

--- src/main/java/umc/ShowHoo/web/book/service/BookCommandServiceImpl.java
+++ src/main/java/umc/ShowHoo/web/book/service/BookCommandServiceImpl.java
@@ -118,7 +118,6 @@ public class BookCommandServiceImpl implements BookCommandService {
             return null;
         }
 
-        book.setStatus(BookStatus.WATCHED);
         book.setDetail(BookDetail.WATCHED);
         return bookRepository.save(book);
     }
