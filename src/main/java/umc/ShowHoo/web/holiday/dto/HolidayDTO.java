b32b664abfff01401f4aabf48fa65c6133757391
hyesuhan <heasoo88@gmail.com>
2024-08-19 09:03:05 +0900

    fix: 대관 가능 날짜 list 오류 수정

---
+++ src/main/java/umc/ShowHoo/web/holiday/dto/HolidayDTO.java
@@ -0,0 +1,14 @@
+package umc.ShowHoo.web.holiday.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HolidayDTO {
+    private LocalDate date;
+}
