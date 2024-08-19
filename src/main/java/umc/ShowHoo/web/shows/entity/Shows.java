3b04b57d5a81b048838e9322208a5e8ef1efe3ef
hyoinkang <103008960+hyoinkang@users.noreply.github.com>
2024-08-19 23:00:27 +0900

    Revert "[feat]각 공연자 별 추가 장비 receipt & [fix]날짜로 대관 확인 시 performer poster과 타이틀 …" (#130)
    This reverts commit fed674d01d096c6fc755397a396031d87d6ca381.

--- src/main/java/umc/ShowHoo/web/shows/entity/Shows.java
+++ src/main/java/umc/ShowHoo/web/shows/entity/Shows.java
@@ -13,7 +13,6 @@ import umc.ShowHoo.web.showsDescription.entity.ShowsDescription;
 import umc.ShowHoo.web.showsPrefer.entity.ShowsPrefer;
 
 import umc.ShowHoo.web.rentalFile.entity.RentalFile;
-import umc.ShowHoo.web.spaceApply.entity.SpaceApply;
 
 @Entity
 @Getter @Setter
@@ -59,7 +58,4 @@ public class Shows {
 
     @OneToOne(mappedBy = "shows")
     private ShowsDescription showsDescription;
-
-    @ManyToOne @JoinColumn(name = "spaceApply_Id")
-    private SpaceApply spaceApply;
 }
