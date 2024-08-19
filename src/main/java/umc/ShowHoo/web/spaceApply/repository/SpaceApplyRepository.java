d7a1fc042c534ad72801a0a6cb254e0ab90a2b92
hyoinkang <103008960+hyoinkang@users.noreply.github.com>
2024-08-19 23:00:27 +0900

    Revert "[feat]각 공연자 별 추가 장비 receipt & [fix]날짜로 대관 확인 시 performer poster과 타이틀 …" (#130)
    This reverts commit fed674d01d096c6fc755397a396031d87d6ca381.

--- src/main/java/umc/ShowHoo/web/spaceApply/repository/SpaceApplyRepository.java
+++ src/main/java/umc/ShowHoo/web/spaceApply/repository/SpaceApplyRepository.java
@@ -2,8 +2,6 @@ package umc.ShowHoo.web.spaceApply.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import umc.ShowHoo.web.performer.entity.Performer;
-import umc.ShowHoo.web.space.entity.Space;
 import umc.ShowHoo.web.spaceApply.entity.SpaceApply;
 
 import javax.swing.text.html.Option;
@@ -14,12 +12,7 @@ import java.util.Optional;
 @Repository
 public interface SpaceApplyRepository extends JpaRepository<SpaceApply, Long> {
     List<SpaceApply> findByPerformerId(Long performerId);
-
-
-    Optional<SpaceApply> findBySpaceAndPerformer(Space space, Performer performer);
-
     List<SpaceApply> findBySpaceIdAndStatusIn(Long spaceId, List<Integer> statuses);
-    List<SpaceApply> findBySpaceIdAndDate(Long spaceId, LocalDate date);
-
+    Optional<SpaceApply> findBySpaceIdAndDate(Long spaceId, LocalDate date);
     boolean existsBySpaceIdAndPerformerId(Long spaceId, Long performerId);
 }
