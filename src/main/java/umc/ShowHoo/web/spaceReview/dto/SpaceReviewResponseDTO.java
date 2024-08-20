4118afbc52319706641a45cab71c77101e5e5328
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-08-20 16:22:30 +0900

    feat: 리뷰 조회 api 반환값 추가 #47

--- src/main/java/umc/ShowHoo/web/spaceReview/dto/SpaceReviewResponseDTO.java
+++ src/main/java/umc/ShowHoo/web/spaceReview/dto/SpaceReviewResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class SpaceReviewResponseDTO {
@@ -29,6 +31,10 @@ public class SpaceReviewResponseDTO {
         private double grade;
         private String content;
         private List<SpaceReviewAnswerDto> answers;
+        private List<String> imageUrls;  // 이미지 URL 리스트 추가
+        private String memberName;       // 리뷰 작성자 이름 추가
+        private URL memberUrl;           // 리뷰 작성자 프로필 이미지 URL 추가
+        private LocalDateTime updatedAt; // 리뷰 수정 시간 추가
     }
 
     @Builder
@@ -38,5 +44,6 @@ public class SpaceReviewResponseDTO {
     public static class SpaceReviewAnswerDto{
         private Long id;
         private String content;
+        private LocalDateTime updatedAt; // 답변 수정 시간 추가
     }
 }
