0dfd92fccf1ce1b67743017ed6055b6de7d7e475
byj7176@gmail.com <byj7176@gmail.com>
2024-08-21 18:11:22 +0900

    [FEAT] 찜 유무 조회 API에 spacePreferId 추가

--- src/main/java/umc/ShowHoo/web/spacePrefer/repository/SpacePreferRepository.java
+++ src/main/java/umc/ShowHoo/web/spacePrefer/repository/SpacePreferRepository.java
@@ -10,5 +10,7 @@ import java.util.List;
 public interface SpacePreferRepository extends JpaRepository<SpacePrefer, Long> {
     List<SpacePrefer> findByPerformer(Performer performer);
 
+    SpacePrefer findBySpaceAndPerformer(Space space, Performer performer);
+
     Boolean existsBySpaceAndPerformer(Space space, Performer performer);
 }
