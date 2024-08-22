827a8976ebf7c5eaea2ba6d625f54d8e6c6c4c74
hyesuhan <121243582+hyesuhan@users.noreply.github.com>
2024-08-22 23:41:44 +0900

    [FIX] 대관 날짜로 공연장 확인 및 shows 확인 (#154)

--- src/main/java/umc/ShowHoo/web/spaceAdditionalService/repository/SpaceAdditionalServiceRepository.java
+++ src/main/java/umc/ShowHoo/web/spaceAdditionalService/repository/SpaceAdditionalServiceRepository.java
@@ -2,7 +2,9 @@ package umc.ShowHoo.web.spaceAdditionalService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.ShowHoo.web.spaceAdditionalService.entity.SpaceAdditionalService;
+import umc.ShowHoo.web.spaceApply.service.SpaceApplyService;
 
 public interface SpaceAdditionalServiceRepository extends JpaRepository<SpaceAdditionalService, Long> {
         SpaceAdditionalService findBySpaceIdAndTitle(Long spaceId, String title);
+
 }
